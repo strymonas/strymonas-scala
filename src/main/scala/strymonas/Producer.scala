3a7b37eb5bf5136340600a9135290e281dfd700f
Aggelos Biboudis <biboudis@gmail.com>
2020-08-04 22:23:52 +0200

    Add for_unfold
    Co-authored-by: Nick Palladinos <nick.palladinos@gmail.com>

--- src/main/scala/strymonas/Producer.scala
+++ src/main/scala/strymonas/Producer.scala
@@ -1,6 +1,7 @@
 package strymonas
 
 import scala.quoted._
+import scala.quoted.util._
 
 type Emit[A] = (A => Expr[Unit]) => Expr[Unit]
 
@@ -9,23 +10,9 @@ trait PullArray[A] {
    def index(st: Expr[Int]): Emit[A]
 }
 
-// trait Var[A] {
-//    def get(using qctx: QuoteContext): Expr[A]
-//    def update(e: Expr[A])(using qctx: QuoteContext): Expr[Unit] 
-// }
-// object Var {
-//    def apply[A: Type](init: Expr[A])(using qctx: QuoteContext) = '{
-//       var x = ${init}
-//       new Var[A] {
-//          def get(using qctx: QuoteContext): Expr[A] = '{x}
-//          def update(e: Expr[A])(using qctx: QuoteContext): Expr[Unit] = '{ x = $e }
-//       }
-//    }
-// }
-
 enum Init[A] {
    case ILet(init: Expr[A]) extends Init[Expr[A]]
-//    case IVar(init: Var[A]) extends Init[Expr[Var[Int]]]
+   case IVar(init: Expr[A]) extends Init[Var[A]]
 } 
 
 enum Producer[A] { 
--- src/main/scala/strymonas/StreamRaw.scala
+++ src/main/scala/strymonas/StreamRaw.scala
@@ -1,6 +1,8 @@
 package strymonas
 
 import scala.quoted._
+import scala.quoted.util._
+
 import imports._
 import imports.Cardinality._
 import scala.compiletime._
@@ -20,24 +22,27 @@ trait StreamRaw extends StreamRawOps {
       }
    }
 
+   private def cseq[A: Type](c1: Expr[Unit], c2: Expr[A]): E[A] = '{
+      ${c1}
+      ${c2}
+   }
+
    private def lets[A: Type, W: Type](x: Expr[A], k: (Expr[A] => Expr[W])): E[W] = '{
       val lv = ${x}
 
       ${k('{lv})}
    }
 
-   // let for_unfold : 'a pull_array -> 'a st_stream =  function {upb;index} ->
-   //  Init (Iref (.<0>., Refl), fun i ->
-   //    Break (.<!(.~i) <= .~upb>.,
-   //     Lin (Unfold (fun k -> index .<! (.~i)>. @@ 
-   //                       fun a -> cseq .<incr .~i>. (k a)))))
-
-   // private def for_unfold[A](pull: PullArray[A])(using QuoteContext): StreamShape[A] = {
-   //    Initializer(
-   //       IVar(Var('{0})), (i: Var[Int]) => {
-   //          Break('{ ${ i.get } <= ${ pull.upb() }}, ???)
-   //    })
-   // }
+   private def for_unfold[A](pull: PullArray[A])(using QuoteContext): StreamShape[A] = {
+      Initializer(
+         IVar('{0}), (i: Var[Int]) => {
+            Break('{ ${ i.get } <= ${ pull.upb() }}, 
+               Linear(Unfold((k: A => Expr[Unit]) => 
+                  pull.index(i.get)((a: A) => cseq(
+                     i.update('{ ${i.get} + 1}),
+                     k(a)
+                  )))))})
+   }
 
    def foldRaw[A: Type](consumer: A => Expr[Unit], st: StreamShape[A]): E[Unit] = {
 
@@ -55,7 +60,6 @@ trait StreamRaw extends StreamRawOps {
          st match {
             case Initializer(ILet(i) /*: Init[Expr[a]]*/, sk /*: (Expr[a] => StreamShape[A])*/): StreamShape[A] => '{
                // TODO: remove cast and/or report on Dotty
-               
                val z = ${i.asInstanceOf[Expr[Any]]} 
 
                ${loop[A](bp, consumer, sk.asInstanceOf[Expr[Any] => StreamShape[A]]('{z}))} 
