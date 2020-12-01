ef50ba743652de5b4e34642f0e235d09650b79e9
moatom <tomozx8@gmail.com>
2020-12-02 00:15:07 +0900

    Refactor Cde & Code

--- src/main/scala/strymonas/Cde.scala
+++ src/main/scala/strymonas/Cde.scala
@@ -5,87 +5,70 @@ import scala.quoted.util._
 
 import scala.language.implicitConversions
 
-trait Cde {
-   type Cde[A]
-   type Var[A]
+trait BoolCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
 
-   implicit def toExpr[A](x: Cde[A]): Expr[A]
-   implicit def ofExpr[A](x: Expr[A]): Cde[A]
-
-   def inj[T: Liftable](c1: T)(using QuoteContext): Cde[T]
-
-   def letl[A: Type, W: Type](x: Cde[A])(k: (Cde[A] => Cde[W]))(using QuoteContext): Cde[W]
-   def letVar[A: Type, W: Type](x: Cde[A])(k: (Var[A] => Cde[W]))(using QuoteContext): Cde[W]
-
-   def seq[A: Type](c1: Cde[Unit], c2: Cde[A])(using QuoteContext): Cde[A]
-   def unit(using QuoteContext): Cde[Unit]
-
-   // Booleans
    def bool(c1: Boolean)(using QuoteContext): Cde[Boolean]
    def not(c1: Cde[Boolean])(using QuoteContext): Cde[Boolean]
    def land(c1: Cde[Boolean], c2: Cde[Boolean])(using QuoteContext): Cde[Boolean]
    def  lor(c1: Cde[Boolean], c2: Cde[Boolean])(using QuoteContext): Cde[Boolean]
-   implicit class BoolCde(val c1: Cde[Boolean]) {
+
+   implicit class BoolOps(val c1: Cde[Boolean]) {
       def &&(c2: Cde[Boolean])(using QuoteContext): Cde[Boolean] = land(c1, c2)
       def ||(c2: Cde[Boolean])(using QuoteContext): Cde[Boolean] =  lor(c1, c2)
    }
+}
 
-   // Numbers
-   def imin(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int]
-   // def imax(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int]
 
-   trait NumCde[A <: AnyVal] {
-      def add(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A]
-      def sub(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A]
-      def mul(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A]
-      def div(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A]
-      def modf(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[A]
-
-      def  lt(c1: Cde[A], c2: Cde[A])(using QuoteContext):     Cde[Boolean]
-      def  gt(c1: Cde[A], c2: Cde[A])(using QuoteContext):     Cde[Boolean]
-      def leq(c1: Cde[A], c2: Cde[A])(using QuoteContext):     Cde[Boolean]
-      def geq(c1: Cde[A], c2: Cde[A])(using QuoteContext):     Cde[Boolean]
-      def eq_temp(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean]
-   }
+trait IntCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
 
    def int(c1: Int)(using QuoteContext): Cde[Int]
-   val int_funs: NumCde[Int]
+   def imin(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int]
+}
+
+trait LongCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
 
    def long(c1: Long)(using QuoteContext): Cde[Long]
-   val long_funs: NumCde[Long]
+}
 
-   implicit class IntOps(val c1: Cde[Int]) {
-      def +(c2: Cde[Int])(using QuoteContext):   Cde[Int] = int_funs.add(c1, c2)
-      def -(c2: Cde[Int])(using QuoteContext):   Cde[Int] = int_funs.sub(c1, c2)
-      def *(c2: Cde[Int])(using QuoteContext):   Cde[Int] = int_funs.mul(c1, c2)
-      def /(c2: Cde[Int])(using QuoteContext):   Cde[Int] = int_funs.div(c1, c2)
-      def mod(c2: Cde[Int])(using QuoteContext): Cde[Int] = int_funs.modf(c1, c2)
+trait NumCde[A <: AnyVal, C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
 
-      def <(c2: Cde[Int])(using QuoteContext):   Cde[Boolean] =  int_funs.lt(c1 ,c2)
-      def >(c2: Cde[Int])(using QuoteContext):   Cde[Boolean] =  int_funs.gt(c1 ,c2)
-      def <=(c2: Cde[Int])(using QuoteContext):  Cde[Boolean] =  int_funs.leq(c1, c2)
-      def >=(c2: Cde[Int])(using QuoteContext):  Cde[Boolean] =  int_funs.geq(c1, c2)
-      def ===(c2: Cde[Int])(using QuoteContext): Cde[Boolean] =  int_funs.eq_temp(c1, c2)
-   }
+   def  add(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[A]
+   def  sub(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[A]
+   def  mul(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[A]
+   def  div(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[A]
+   def modf(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[A]
 
-   implicit class LongOps(val c1: Cde[Long]) {
-      def +(c2: Cde[Long])(using QuoteContext):   Cde[Long] = long_funs.add(c1, c2)
-      def -(c2: Cde[Long])(using QuoteContext):   Cde[Long] = long_funs.sub(c1, c2)
-      def *(c2: Cde[Long])(using QuoteContext):   Cde[Long] = long_funs.mul(c1, c2)
-      def /(c2: Cde[Long])(using QuoteContext):   Cde[Long] = long_funs.div(c1, c2)
-      def mod(c2: Cde[Long])(using QuoteContext): Cde[Long] = long_funs.modf(c1, c2)
+   def      lt(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean]
+   def      gt(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean]
+   def     leq(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean]
+   def     geq(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean]
+   def eq_temp(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean]
+}
 
-      def <(c2: Cde[Long])(using QuoteContext):   Cde[Boolean] =  long_funs.lt(c1 ,c2)
-      def >(c2: Cde[Long])(using QuoteContext):   Cde[Boolean] =  long_funs.gt(c1 ,c2)
-      def <=(c2: Cde[Long])(using QuoteContext):  Cde[Boolean] =  long_funs.leq(c1, c2)
-      def >=(c2: Cde[Long])(using QuoteContext):  Cde[Boolean] =  long_funs.geq(c1, c2)
-      def ===(c2: Cde[Long])(using QuoteContext): Cde[Boolean] =  long_funs.eq_temp(c1, c2)
-   }
+
+trait BasicCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
+
+   def letl[A: Type, W: Type](x: Cde[A])(k: (Cde[A] => Cde[W]))(using QuoteContext): Cde[W]
+   def letVar[A: Type, W: Type](x: Cde[A])(k: (Var[A] => Cde[W]))(using QuoteContext): Cde[W]
 
    // Control operators
+   def seq[A: Type](c1: Cde[Unit], c2: Cde[A])(using QuoteContext): Cde[A]
+   def unit(using QuoteContext): Cde[Unit]
+
    def cond[A: Type](cnd: Cde[Boolean], bt: Cde[A], bf: Cde[A])(using QuoteContext): Cde[A]
    def if_(cnd: Cde[Boolean], bt: Cde[Unit], bf: Cde[Unit])(using QuoteContext): Cde[Unit]
    def if1(cnd: Cde[Boolean], bt: Cde[Unit])(using QuoteContext): Cde[Unit]
+
    def for_(upb: Cde[Int], guard: Option[Cde[Boolean]], body: Cde[Int] => Cde[Unit])(using QuoteContext): Cde[Unit]
    def cloop[A: Type](k: A => Cde[Unit], bp: Option[Cde[Boolean]], body: ((A => Cde[Unit]) => Cde[Unit]))(using QuoteContext): Cde[Unit]
    def while_(goon: Cde[Boolean])(body: Cde[Unit])(using QuoteContext): Cde[Unit]
@@ -93,6 +76,7 @@ trait Cde {
    //  Variables (Reference cells)
    def assign[A](c1: Var[A], c2: Cde[A])(using QuoteContext): Cde[Unit]
    def dref[A](x: Var[A])(using QuoteContext): Cde[A]
+
    def incr(i: Var[Int])(using QuoteContext): Cde[Unit]
    def decr(i: Var[Int])(using QuoteContext): Cde[Unit]
 
@@ -102,22 +86,68 @@ trait Cde {
    implicit class VarCde[A](val c1: Var[A]) {
       def :=(c2: Cde[A])(using QuoteContext): Cde[Unit] = assign[A](c1, c2)
    }
+}
 
-   // Arrays
+trait ArrayCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
+
+   def int_array[A: Type](arr: Array[Int])(using QuoteContext): Cde[Array[Int]]
    def array_get[A: Type, W: Type](arr: Cde[Array[A]])(i: Cde[Int])(k: (Cde[A] => Cde[W]))(using QuoteContext): Cde[W]
    def array_len[A: Type](arr: Cde[Array[A]])(using QuoteContext): Cde[Int]
    def array_set[A: Type](arr: Cde[Array[A]])(i: Cde[Int])(v: Cde[A])(using QuoteContext): Cde[Unit]
-   def int_array[A: Type](arr: Array[Int])(using QuoteContext): Cde[Array[Int]]
+}
 
-   // Others
+
+trait OtherCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
+
    def pair[A: Type, B: Type](x: Cde[A], y: Cde[B])(using QuoteContext): Cde[Tuple2[A,B]]
    def uninit[A: Type](using QuoteContext): Cde[A]
    def blackhole[A: Type](using QuoteContext): Cde[A]
    def is_static[A: Type](c1: Cde[A])(using QuoteContext): Boolean
    def is_fully_dynamic[A: Type](c1: Cde[A])(using QuoteContext): Boolean
+}
 
-   // Lists
+
+trait ListCde[C[_], V[_]] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
+
    def nil[A: Type]()(using QuoteContext): Cde[List[A]] 
    def cons[A: Type](x: Cde[A], xs: Cde[List[A]])(using QuoteContext): Cde[List[A]]
    def reverse[A: Type](xs: Cde[List[A]])(using QuoteContext): Cde[List[A]] 
 }
+
+
+trait Cde[C[_], V[_]] extends BoolCde[C, V] with   IntCde[C, V] with  LongCde[C, V]
+                                            with BasicCde[C, V] with ArrayCde[C, V]
+                                            with OtherCde[C, V] with  ListCde[C, V] {
+   type Cde[A] = C[A]
+   type Var[A] = V[A]
+
+   implicit def toExpr[A](x: Cde[A]): Expr[A]
+   implicit def ofExpr[A](x: Expr[A]): Cde[A]
+
+   def inj[T: Liftable](c1: T)(using QuoteContext): Cde[T]
+
+   implicit val  IntOp: NumCde[Int, C, V]
+   implicit val LongOp: NumCde[Long, C, V]
+   implicit class NumOps[A <: AnyVal](val c1: Cde[A])(implicit t :NumCde[A, C, V]) {
+      def +(c2: Cde[A])(using QuoteContext):   Cde[A] = t.add(c1, c2)
+      def -(c2: Cde[A])(using QuoteContext):   Cde[A] = t.sub(c1, c2)
+      def *(c2: Cde[A])(using QuoteContext):   Cde[A] = t.mul(c1, c2)
+      def /(c2: Cde[A])(using QuoteContext):   Cde[A] = t.div(c1, c2)
+      def mod(c2: Cde[A])(using QuoteContext): Cde[A] = t.modf(c1, c2)
+
+      def <(c2: Cde[A])(using QuoteContext):   Cde[Boolean] =     t.lt(c1 ,c2)
+      def >(c2: Cde[A])(using QuoteContext):   Cde[Boolean] =     t.gt(c1 ,c2)
+      def <=(c2: Cde[A])(using QuoteContext):  Cde[Boolean] =     t.leq(c1, c2)
+      def >=(c2: Cde[A])(using QuoteContext):  Cde[Boolean] =     t.geq(c1, c2)
+      def ===(c2: Cde[A])(using QuoteContext): Cde[Boolean] = t.eq_temp(c1, c2)
+   }
+}
+
+
+// trait CdeEx[C[_], V[_]] extends Cde[C, V] with ...
--- src/main/scala/strymonas/Code.scala
+++ src/main/scala/strymonas/Code.scala
@@ -8,16 +8,12 @@ import scala.language.implicitConversions
 /**
  * The Scala's code generator
  */
-object Code extends Cde {
-   type Cde[A] = Expr[A]
-   type Var[A] = scala.quoted.util.Var[A]
-
+object Code extends Cde[Expr, scala.quoted.util.Var] {
    implicit def toExpr[A](x: Cde[A]): Expr[A] = x
    implicit def ofExpr[A](x: Expr[A]): Cde[A] = x
 
-   def inj[T: Liftable](c1: T)(using QuoteContext): Cde[T] = Expr(c1)
-
    // utils
+   def inj[T: Liftable](c1: T)(using QuoteContext): Cde[T] = Expr(c1)
    def foldOpt[Z, A](f: Z => A => Z, z: Z, value: Option[A]): Z  = {
       value match {
          case None => z
@@ -45,7 +41,6 @@ object Code extends Cde {
 
    def letl[A: Type, W: Type](x: Cde[A])(k: (Cde[A] => Cde[W]))(using QuoteContext): Cde[W] = '{
       val lv = ${x}
-
       ${k('{lv})}
    }
 
@@ -59,34 +54,21 @@ object Code extends Cde {
 
    def unit(using QuoteContext): Cde[Unit] = '{}
 
+
    // Booleans
    def bool(c1: Boolean)(using QuoteContext): Cde[Boolean] = Expr(c1)
-
-   def not(c1: Cde[Boolean])(using QuoteContext): Cde[Boolean] = '{
-      ! ${c1}
-   }
-
-   def land(c1: Cde[Boolean], c2: Cde[Boolean])(using QuoteContext): Cde[Boolean] = '{
-      ${c1} && ${c2}
-   }
-
-   def  lor(c1: Cde[Boolean], c2: Cde[Boolean])(using QuoteContext): Cde[Boolean] = '{
-      ${c1} || ${c2}
-   }
+   def not(c1: Cde[Boolean])(using QuoteContext): Cde[Boolean] = 
+      '{! ${c1}}
+   def land(c1: Cde[Boolean], c2: Cde[Boolean])(using QuoteContext): Cde[Boolean] = 
+      '{${c1} && ${c2}}
+   def  lor(c1: Cde[Boolean], c2: Cde[Boolean])(using QuoteContext): Cde[Boolean] = 
+      '{${c1} || ${c2}}
 
 
    // Numbers
-   def imin(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int] = {
-      //TODO: ported Oleg's, need to check perf
-      cond('{ ${c1} < ${c2} }, c1, c2)
-   }
-   // def imax(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int] = {
-   //    //TODO: ported Oleg's, need to check perf
-   //    cond('{ ${c1} > ${c2} }, c1, c2)
-   // }
-
-   class IntCode extends NumCde[Int] {
+   class IntCode extends NumCde[Int, Cde, Var] {
       type A = Int
+
       def add(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A] = 
          '{${c1} + ${c2}}
       def sub(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A] = 
@@ -110,8 +92,9 @@ object Code extends Cde {
          '{${c1} == ${c2}}
    }
 
-   class LongCode extends NumCde[Long] {
+   class LongCode extends NumCde[Long, Cde, Var] {
       type A = Long
+
       def add(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A] = 
          '{${c1} + ${c2}}
       def sub(c1: Cde[A], c2: Cde[A])(using QuoteContext):  Cde[A] = 
@@ -134,46 +117,47 @@ object Code extends Cde {
       def eq_temp(c1: Cde[A], c2: Cde[A])(using QuoteContext): Cde[Boolean] = 
          '{${c1} == ${c2}}
    }
+
+   implicit val  IntOp: NumCde[ Int, Cde, Var]  =  IntCode()
+   implicit val LongOp: NumCde[Long, Cde, Var] = LongCode()
 
    def int(c1: Int)(using QuoteContext): Cde[Int] = Expr(c1)
-   val int_funs: NumCde[Int] = IntCode()
+
+   // TODO: ported Oleg's, need to check perf
+   def imin(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int] = 
+      {cond('{ ${c1} < ${c2} }, c1, c2)}
+
+   // def imax(c1: Cde[Int], c2: Cde[Int])(using QuoteContext): Cde[Int] = 
+   //    {cond('{ ${c1} > ${c2} }, c1, c2)}
 
    def long(c1: Long)(using QuoteContext): Cde[Long] = Expr(c1)
-   val long_funs: NumCde[Long] = LongCode()
 
 
    // Control operators
-   def cond[A: Type](cnd: Cde[Boolean], bt: Cde[A], bf: Cde[A])(using QuoteContext): Cde[A] = '{
-      if(${cnd}) then ${bt} else ${bf}
-   }
-
-   def if_(cnd: Cde[Boolean], bt: Cde[Unit], bf: Cde[Unit])(using QuoteContext): Cde[Unit] = '{
-      if(${cnd}) then ${bt} else ${bf}
-   }
-
-   def if1(cnd: Cde[Boolean], bt: Cde[Unit])(using QuoteContext): Cde[Unit] = '{
-      if(${cnd}) then ${bt}
-   }
+   def cond[A: Type](cnd: Cde[Boolean], bt: Cde[A], bf: Cde[A])(using QuoteContext): Cde[A] = 
+      '{if(${cnd}) then ${bt} else ${bf}}
+   def if_(cnd: Cde[Boolean], bt: Cde[Unit], bf: Cde[Unit])(using QuoteContext): Cde[Unit] = 
+      '{if(${cnd}) then ${bt} else ${bf}}
+   def if1(cnd: Cde[Boolean], bt: Cde[Unit])(using QuoteContext): Cde[Unit] = 
+      '{if(${cnd}) then ${bt}}
 
    def for_(upb: Cde[Int],
             guard: Option[Cde[Boolean]],
             body: Cde[Int] => Cde[Unit])(using QuoteContext): Cde[Unit] = {
       guard match {
       case Some(g) =>
-         '{
-         var i = 0
-         while(i <= ${upb} && ${g}) {
-               ${body('i)}
-               i = i + 1
-         }
-         }
-      case None => // for-loop is emittable
-         '{
-               var i = 0
-               while(i <= ${upb}) {
+         '{ var i = 0
+            while(i <= ${upb} && ${g}) {
                   ${body('i)}
                   i = i + 1
-               }
+            }
+         }
+      case None => // for-loop is emittable
+         '{ var i = 0
+            while(i <= ${upb}) {
+               ${body('i)}
+               i = i + 1
+            }
          }
       }
    }
@@ -181,9 +165,10 @@ object Code extends Cde {
    def cloop[A: Type](k: A => Cde[Unit],
                      bp: Option[Cde[Boolean]],
                      body: ((A => Cde[Unit]) => Cde[Unit]))(using QuoteContext): Cde[Unit] = {
-      Var(bool(true)) { again => 
+      letVar(bool(true)) { again => 
          while_(foldOpt[Cde[Boolean], Cde[Boolean]](x => z => '{ ${x} && ${z}}, again.get, bp))(
-                  body(x => seq(again.update(bool(false)), k(x))))
+                  body(x => seq(again.update(bool(false)), k(x)))
+         )
       }
    }
 
@@ -193,57 +178,33 @@ object Code extends Cde {
       }
    }
 
-   //  Reference cells?
-   def assign[A](c1: Var[A], c2: Cde[A])(using QuoteContext): Cde[Unit] = {
-      c1.update(c2)
-   }
 
-   def dref[A](x: Var[A])(using QuoteContext): Cde[A] = {
-      x.get
-   }
+   //  Reference cells
+   def assign[A](c1: Var[A], c2: Cde[A])(using QuoteContext): Cde[Unit] = c1.update(c2)
+   def dref[A](x: Var[A])(using QuoteContext): Cde[A] = x.get
+   def incr(i: Var[Int])(using QuoteContext): Cde[Unit] = i.update('{${dref(i)} + ${inj(1)}})
+   def decr(i: Var[Int])(using QuoteContext): Cde[Unit] = i.update('{${dref(i)} - ${inj(1)}})
+   def long_incr(i: Var[Long])(using QuoteContext): Cde[Unit] = i.update('{${dref(i)} + ${inj(1)}})
+   def long_decr(i: Var[Long])(using QuoteContext): Cde[Unit] = i.update('{${dref(i)} - ${inj(1)}})
 
-   def incr(i: Var[Int])(using QuoteContext): Cde[Unit] = {
-      i.update(dref(i) + inj(1))
-   }
-   def decr(i: Var[Int])(using QuoteContext): Cde[Unit] = {
-      i.update(dref(i) - inj(1))
-   }
-
-   def long_incr(i: Var[Long])(using QuoteContext): Cde[Unit] = {
-      i.update(dref(i) + inj(1))
-   }
-   def long_decr(i: Var[Long])(using QuoteContext): Cde[Unit] = {
-      i.update(dref(i) - inj(1))
-   }
 
    // Arrays
-   def array_get[A: Type, W: Type](arr: Cde[Array[A]])
-                                  (i: Cde[Int])
+   def int_array[A: Type](arr: Array[Int])(using QuoteContext): Cde[Array[Int]] = inj(arr)
+   def array_get[A: Type, W: Type](arr: Cde[Array[A]])(i: Cde[Int])
                                   (k: (Cde[A] => Cde[W]))(using QuoteContext): Cde[W] =
       letl('{${arr}.apply{${i}}})(k)
+   def array_len[A: Type](arr: Cde[Array[A]])(using QuoteContext): Cde[Int] =
+      '{${arr}.length}
+   def array_set[A: Type](arr: Cde[Array[A]])(i: Cde[Int])(v: Cde[A])(using QuoteContext): Cde[Unit] =
+      '{${arr}{${i}} = ${v}}
 
-   def array_len[A: Type](arr: Cde[Array[A]])(using QuoteContext): Cde[Int] = '{
-      ${arr}.length
-   }
-
-   def array_set[A: Type](arr: Cde[Array[A]])(i: Cde[Int])(v: Cde[A])(using QuoteContext): Cde[Unit] = '{
-      ${arr}{${i}} = ${v}
-   }
-
-   def int_array[A: Type](arr: Array[Int])(using QuoteContext): Cde[Array[Int]] = inj(arr)
-
+   
    // Others
-   def pair[A: Type, B: Type](x: Cde[A], y: Cde[B])(using QuoteContext): Cde[Tuple2[A,B]] = '{
-      (${x}, ${y})
-   }
-
+   def pair[A: Type, B: Type](x: Cde[A], y: Cde[B])(using QuoteContext): Cde[Tuple2[A,B]] =
+      '{(${x}, ${y})}
    def uninit[A: Type](using QuoteContext): Cde[A] = default(summon[Type[A]])
-
    def blackhole[A: Type](using QuoteContext): Cde[A] = '{throw new Exception("BH")}
-   // def blackhole[A: Type](using QuoteContext): Cde[A] = '{???}
-
    def is_static[A: Type](c1: Cde[A])(using QuoteContext): Boolean = false
-
    def is_fully_dynamic[A: Type](c1: Cde[A])(using QuoteContext): Boolean = true
 
    def nil[A: Type]()(using QuoteContext): Cde[List[A]] = '{ Nil }
