d1ad05fe64c7c730705d156672eacc01c82f581b
Aggelos Biboudis <biboudis@gmail.com>
2020-12-10 00:25:11 +0100

    Some more combinators before actual derivation

--- src/test/scala/DerivationTest.scala
+++ src/test/scala/DerivationTest.scala
@@ -10,10 +10,25 @@ class DerivationTest {
     trait Stream0 { 
         type Stream[A]
 
+        // Producers
         def ofArr[A](arr: Array[A]): Stream[A]
- 
-        def mapAccum[A, B, Z](f: Z => A => (B, Z))(st: Z)(s: Stream[A]): Stream[B]
+        def iota(i: Int): Stream[Int]
+        def fromTo(step: Int, from: Int, to: Int): Stream[Int]
+        def unfold[A, Z](step: Z => Option[(A, Z)])(z: Z): Stream[A]
 
+        // Transformers
+        def map[A, B](f: A => B)(s: Stream[A]): Stream[B]
+        def filter[A](f: A => Boolean)(s: Stream[A]): Stream[A]
+        def flatMap[A, B](f: A => Stream[B])(s: Stream[A]): Stream[B]
+
+        def mapAccum[A, B, Z](f: Z => A => (B, Z))(st: Z)(s: Stream[A]): Stream[B]
+        def take[A](n: Int)(s: Stream[A]): Stream[A]
+        def take_while[A](f: A => Boolean)(s1: Stream[A]): Stream[A]
+        def drop[A](n: Int)(s: Stream[A]): Stream[A]
+        def drop_while[A](f: A => Boolean)(s1: Stream[A]): Stream[A]
+        def zipWith[A, B, C](f: A => B => C)(s1: Stream[A])(s2: Stream[B]): Stream[C]
+
+        // Consumers
         def observe[A](limit: Int)(s: Stream[A]): List[A]
     }
 
@@ -28,6 +43,7 @@ class DerivationTest {
     object Stream0Denot extends Stream0 {
         type Stream[A] = SkipStream[A]
 
+        // Producers
         def ofArr[A](arr: Array[A]): Stream[A] = {
             def gen(i: Int): SkipStream[A] = {
                 if i >= arr.length then Nil()
@@ -37,6 +53,27 @@ class DerivationTest {
             gen(0)
         }
 
+        def iota(i: Int): Stream[Int] = {
+            Cons(i, () => iota(i + 1))
+        }
+
+        def fromTo(step: Int, from: Int, to: Int): Stream[Int] = {
+            if(step >= 0) then
+                if(from < to) Cons(from, () => fromTo(step, from + step, to))
+                else Nil()
+            else 
+                if(from > to) Cons(from, () => fromTo(step, from + step, to))
+                else Nil()              
+        }
+
+        def unfold[A, Z](step: Z => Option[(A, Z)])(z: Z): Stream[A] = {
+            step(z) match {
+                case None => Nil()
+                case Some(a, z) => Cons(a, () => unfold(step)(z))
+            }
+        }
+
+        // Transfomers
         def mapAccum[A, B, Z](f: Z => A => (B, Z))(st: Z)(s: Stream[A]): Stream[B] = {
             s match {
                 case Nil() => Nil()
@@ -63,6 +100,87 @@ class DerivationTest {
             }
         }
 
+        def filter[A](f: A => Boolean)(s: Stream[A]): Stream[A] = {
+            s match {
+                case Nil() => Nil()
+                case Cons(x, t) => 
+                    if(f(x)) Cons(x, () => filter(f)(t()))
+                    else Skip(() => filter(f)(t()))
+                case Skip(t) => 
+                    Skip(() => filter(f)(t()))
+            }
+        }
+
+        def take[A](n: Int)(s: Stream[A]): Stream[A] = {
+            s match {
+                case Nil() => Nil()
+                case Cons(x, t) => 
+                    if(n > 0) Cons(x, () => take(n)(t()))
+                    else Nil()
+                case Skip(t) => 
+                    Skip(() => take(n)(t()))
+            }
+        }
+
+        def take_while[A](f: A => Boolean)(s: Stream[A]): Stream[A] = {
+            s match {
+                case Nil() => Nil()
+                case Cons(x, t) => 
+                    if(f(x)) Cons(x, () => take_while(f)(t()))
+                    else Nil()
+                case Skip(t) => 
+                    Skip(() => take_while(f)(t()))
+            }
+        }
+
+        def drop[A](n: Int)(s: Stream[A]): Stream[A] = {
+            s match {
+                case Nil() => Nil()
+                case Cons(x, t) => 
+                    if(n > 0) Skip(() => drop(n - 1)(t()))
+                    else Cons(x, t)
+                case Skip(t) => 
+                    Skip(() => drop(n)(t()))
+            }
+        }
+
+        def drop_while[A](f: A => Boolean)(s: Stream[A]): Stream[A] = {
+            s match {
+                case Nil() => Nil()
+                case Cons(x, t) => 
+                    if(f(x)) Skip(() => drop_while(f)(t()))
+                    else Cons(x, t)
+                case Skip(t) => 
+                    Skip(() => drop_while(f)(t()))
+            }
+        }
+
+        def flatMap[A, B](f: A => Stream[B])(s: Stream[A]): Stream[B] = {
+            s match {
+                case Nil() => Nil()
+                case Skip(t) => 
+                    Skip(() => flatMap(f)(t()))
+                case Cons(x, t) => 
+                    def inner(s: Stream[B])(tt: Stream[A]): Stream[B] =
+                        s match {
+                            case Nil() => Skip(() => flatMap(f)(tt))
+                            case Skip(t) => Skip(() => inner(t())(tt))
+                            case Cons(x, t) => Cons(x, () => inner(t())(tt))
+                        }
+
+                    inner(f(x))(t())
+            }
+        }
+
+        def zipWith[A, B, C](f: A => B => C)(s1: Stream[A])(s2: Stream[B]): Stream[C] = {
+            (s1, s2) match {
+                case (Nil(), Nil())             => Nil()
+                case (Skip(t), s2)              => Skip(() => zipWith(f)(t())(s2))
+                case (s1, Skip(t))              => Skip(() => zipWith(f)(s1)(t()))
+                case (Cons(x, t1), Cons(y, t2)) => Cons(f(x)(y), () => zipWith(f)(t1())(t2()))
+            }
+        }
+
         def observe[A](limit: Int)(s: Stream[A]): List[A] = {
             s match { 
                 case Nil() => scala.Nil
@@ -74,8 +192,27 @@ class DerivationTest {
                     val s = t() 
                     observe(limit)(s)
             }
-        } 
+        }
     }
+
+    trait Stream1 { 
+        type Stream[A]
+  
+        // Producers
+        def pullArray[A](upb: Int)(f: Int => A): Stream[A]
+        def fromTo(ifrom: Int, step: Int): Stream[Int]
+        def unfold[A, Z](step: Z => Option[(A, Z)])(z: Z): Stream[A]
+
+        // Transformers
+        def mapAccum[A, B, Z](f: Z => A => (B, Z))(st: Z)(s: Stream[A]): Stream[B]
+        def flatMap[A, B, Z](f: Z => A => Stream[(B, Z)])(z: Z)(s: Stream[A]): Stream[B]
+        def filter[A](f: A => Boolean)(s: Stream[A]): Stream[A]
+        def take_while[A](f: A => Boolean)(s1: Stream[A]): Stream[A]
+        def zip[A, B](s1: Stream[A])(s2: Stream[B]): Stream[(A, B)]
+
+        // Consumers
+        def observe[A](limit: Int)(s: Stream[A]): List[A]
+    }   
 
     @Test def direct_tests() = {
         import Stream0Denot._
